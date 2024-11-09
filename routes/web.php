7d707468503ad2fe9801c2a80e57b6c3e4fa3f27
YamilRA <jyra010920@gmail.com>
2024-11-09 13:02:07 -0600

    modif web

--- routes/web.php
+++ routes/web.php
@@ -24,22 +24,18 @@ Route :: get('/alumno/grupos', [AlumnoController:: class , 'grupos']);
 Route :: get('/alumno/finanzas', [AlumnoController:: class , 'finanzas']);
 
 
-<<<<<<< HEAD
+
 Route::get('/admin/users', function () {
     return view('Admin/UsersAdmin');
 });
 
-<<<<<<< HEAD
 Route::get('/admin/users', [UserController::class, 'create'])->name('admin.users.create');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
-=======
 Route::get('/molde',[LayoutController::class,'molde']);
 
-=======
+
 Route::get('/login/admin', [AdminController::class,'inicioAdmin']);
 Route::get ('/login/admin/addUser',[AdminController::class,'users']);
 Route::get ('/login/admin/addUser/userAdmin',[AdminController::class,'addAdmin']);
 Route::get ('/login/admin/addUser/userProfe',[AdminController::class,'addProfesor']);
 Route::get ('/login/admin/addUser/userAlumno',[AdminController::class,'addAlumno']);
->>>>>>> 9216cce57c0e7c4263113a47b05bc668293cf531
->>>>>>> beta
